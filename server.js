499dce323ce6d70d672087cb3fbc368fc14680cf
Sarika Matthew <pragmattika@Sarika-Matthews-MacBook-Pro-2.local>
2018-03-31 12:43:58 -0400

    refator: set home path to react index.html

--- server.js
+++ server.js
@@ -12,9 +12,10 @@ var PORT = process.env.PORT || 3001;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 // Serve up static assets
-//app.use(express.static(path.join(__dirname, "client/build/")));
+app.use(express.static(path.join(__dirname, "client/build/")));
+
 app.get('/', function(req, res){
-	res.send('react app will go here..')
+	res.sendFile(path.join(__dirname, "../client/build/index.html"));
 })
 // Add routes, both API and view
 app.use(routes);
